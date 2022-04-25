0cef13133c61236596fcddf562a1c8d87ed9223b
GuillaumeCARRA <carraguillaume@gmail.com>
2022-04-25 13:05:44 +0200

    add login and register component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Home from './components/Home';
 import ProductList from './components/ProductList';
 import Product from './components/Product';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
       {/* <ProductList /> */}
-      <Product />
+      {/* <Product /> */}
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
